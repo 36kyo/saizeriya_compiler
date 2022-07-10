b5ca137b2dcc8789257bd9e575a7f482a8829226
36kyo <kyotar@gmail.com>
2022-07-10 18:34:17 +0900

    Add files via upload

---
+++ saizeriya.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+from pathlib import Path
+import argparse
+import csv
+
+menufilepath = Path('saizeriya_menu.txt')
+
+
+def printallmenu():
+    with open(menufilepath, encoding='utf-8', mode='r') as f:
+        print(f.read())
+
+def convert(in_txt: str, iscompile: bool):
+    with open(menufilepath, encoding='utf-8', mode='r') as f:
+        menulist = csv.reader(f, delimiter=',')
+        menulistsorted = sorted(
+            list(menulist).copy(),
+            key=lambda x: len(
+                x[1]),
+            reverse=True)
+        # print(menulistsorted)
+
+        out_txt = in_txt
+        if iscompile:
+            for i in range(len(menulistsorted)):
+                out_txt = out_txt.replace(menulistsorted[i][1], menulistsorted[i][0])
+        else:  # decompile
+            for i in range(len(menulistsorted)):
+                out_txt = out_txt.replace(menulistsorted[i][0], menulistsorted[i][1])
+        print(out_txt)
+
+
+def compile(in_txt: str):
+    convert(in_txt=in_txt, iscompile=True)
+
+
+def decompile(in_txt: str):
+    convert(in_txt=in_txt, iscompile=False)
+
+
+def main():
+    psr = argparse.ArgumentParser()
+    psr.add_argument('-a', '--all', help="print all menu")
+    psr.add_argument('-c', '--compile', default="", help="convert menu to menu code")
+    psr.add_argument('-d', '--decompile', default="", help="convert menu code to menu")
+    args = psr.parse_args()
+
+    # if args.all:
+    #     printallmenu()
+    if len(args.compile) > 0:
+        compile(args.compile)
+    if len(args.decompile) > 0:
+        decompile(args.decompile)
+    # test
+    # compile("カルボナーラ大盛")
+    # decompile("PA55")
+    # compile("バッファローモッツァレラのピザWチーズ、エビクリームグラタン、カルボナーラ大盛(｀・ω・´)")
+
+
+if __name__ == '__main__':
+    main()
